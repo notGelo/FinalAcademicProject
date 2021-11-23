348999f4d73497e09aba2ec68b244257deed403a
J.Go <jaceallyana.go.iics@ust.edu.ph>
2021-11-23 10:34:38 +0800

    home page with background

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,37 +15,45 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Scaffold(
         appBar: new AppBar(backgroundColor: Color.fromRGBO(215, 215, 215, 1)),
         drawer: NavBar(),
-        body: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              flex: 4,
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Text(
-                  'Grubhie',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 50.0,
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/bg_home.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                flex: 4,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Text(
+                    'Grubhie',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 50.0,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              flex: 6,
-              child: Container(
-                margin: EdgeInsets.fromLTRB(13, 15, 13, 0),
-                child: TextField(
-                  style: TextStyle(
-                    color: Colors.black,
+              Expanded(
+                flex: 6,
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(13, 15, 13, 0),
+                  child: TextField(
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                    decoration: InputDecoration(
+                        fillColor: Color.fromRGBO(230, 230, 230, 1),
+                        filled: true),
                   ),
-                  decoration: InputDecoration(
-                      fillColor: Color.fromRGBO(230, 230, 230, 1),
-                      filled: true),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
