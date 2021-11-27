9c32de3faf46af50a81a5d02cb956ce1a4b40f42
notGeloFAP <angelo.paligutan.iics@ust.edu.ph>
2021-11-28 00:21:46 +0800

    HEMLO PAPSI

--- lib/provider/theme_provider.dart
+++ lib/provider/theme_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  ThemeMode themeMode = ThemeMode.dark;
+  ThemeMode themeMode = ThemeMode.light;
 
   bool get isDarkMode => themeMode == ThemeMode.dark;
 
